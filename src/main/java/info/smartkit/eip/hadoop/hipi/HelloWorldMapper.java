67da26fb311678f219c89d9b79b3f990d51875cd
yangboz <YoungWelle@gmail.com>
2015-11-04 14:02:43 +0800

    CHANGED:applied the ComputingTheAveragePixelColor example code

--- src/main/java/info/smartkit/eip/hadoop/hipi/HelloWorldMapper.java
+++ src/main/java/info/smartkit/eip/hadoop/hipi/HelloWorldMapper.java
@@ -12,7 +12,38 @@ import java.io.IOException;
  */
 public class HelloWorldMapper extends Mapper<HipiImageHeader, FloatImage, IntWritable, FloatImage> {
     public void map(HipiImageHeader key, FloatImage value, Context context) throws IOException, InterruptedException {
+        // Verify that image was properly decoded, is of sufficient size, and has three color channels (RGB)
+        if (value != null && value.getWidth() > 1 && value.getHeight() > 1 && value.getNumBands() == 3) {
 
+            // Get dimensions of image
+            int w = value.getWidth();
+            int h = value.getHeight();
+
+            // Get pointer to image data
+            float[] valData = value.getData();
+
+            // Initialize 3 element array to hold RGB pixel average
+            float[] avgData = {0, 0, 0};
+
+            // Traverse image pixel data in raster-scan order and update running average
+            for (int j = 0; j < h; j++) {
+                for (int i = 0; i < w; i++) {
+                    avgData[0] += valData[(j * w + i) * 3 + 0]; // R
+                    avgData[1] += valData[(j * w + i) * 3 + 1]; // G
+                    avgData[2] += valData[(j * w + i) * 3 + 2]; // B
+                }
+            }
+
+            // Create a FloatImage to store the average value
+            FloatImage avg = new FloatImage(1, 1, 3, avgData);
+
+            // Divide by number of pixels in image
+            avg.scale(1.0f / (float) (w * h));
+
+            // Emit record to reducer
+            context.write(new IntWritable(1), avg);
+
+        } // If (value != null...
     }
 
 }
--- src/main/java/info/smartkit/eip/hadoop/hipi/HelloWorldReducer.java
+++ src/main/java/info/smartkit/eip/hadoop/hipi/HelloWorldReducer.java
@@ -12,6 +12,24 @@ import java.io.IOException;
  */
 public class HelloWorldReducer extends Reducer<IntWritable, FloatImage, IntWritable, Text> {
     public void reduce(IntWritable key, Iterable<FloatImage> values, Context context) throws IOException, InterruptedException {
+        // Create FloatImage object to hold final result
+        FloatImage avg = new FloatImage(1, 1, 3);
 
+        // Initialize a counter and iterate over IntWritable/FloatImage records from mapper
+        int total = 0;
+        for (FloatImage val : values) {
+            avg.add(val);
+            total++;
+        }
+
+        if (total > 0) {
+            // Normalize sum to obtain average
+            avg.scale(1.0f / total);
+            // Assemble final output as string
+            float[] avgData = avg.getData();
+            String result = String.format("Average pixel value: %f %f %f", avgData[0], avgData[1], avgData[2]);
+            // Emit output of job which will be written to HDFS
+            context.write(key, new Text(result));
+        }
     }
 }
